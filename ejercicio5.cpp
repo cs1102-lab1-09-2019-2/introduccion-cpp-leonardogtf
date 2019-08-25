6781cdcf0fb6a0db8904a23c11c8d9f3bf00544b
leonardo-tarazona <54273599+leonardo-tarazona@users.noreply.github.com>
2019-08-25 15:32:25 -0500

    Update ejercicio5.cpp

--- ejercicio5.cpp
+++ ejercicio5.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
+#include <vector>
 using std::cout;
+using std::vector;
 
 int main() {
-
-    vector<int> a {1, 2, 3, 4, 5};
-    for (auto i : a) {
-        // Imprimir los numeros
-    }
+  vector<int> a{1, 2, 3, 4, 5};
+  for (auto i : a) {
+      cout<<i;
+  }
 }
