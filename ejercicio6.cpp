151d5ccceb9340ddce9f79ba8dc3bc2547791f38
leonardo-tarazona <54273599+leonardo-tarazona@users.noreply.github.com>
2019-08-25 15:45:07 -0500

    Update ejercicio6.cpp

--- ejercicio6.cpp
+++ ejercicio6.cpp
@@ -1,13 +1,13 @@
-
-
+#include <iostream>
+#include <vector>
+using std::cout;
+using std::vector;
 int main() {
     vector<vector<int>> v{{1, 2}, {3, 4}, {5, 6}};
     for (auto a : v) {
         for (auto i : a) {
-            // imprimir i
+            cout<<i;
         }
-        // salto de linea
+        cout<<"\n";
     }
 }
-
-
